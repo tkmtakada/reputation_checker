f6454dd7a35d8845e732ae73af9eaeb287111901
Koichi Takato <koichi.taka.0818@gmail.com>
2023-02-12 22:44:27 +0900

    feat: 検索画面の大まかな初期レイアウト作成

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,43 @@
+import { Typography } from '@mui/material';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 import { SearchBox } from './components/SearchBox';
 
-// import styled from 'styled-components';
-
-/*
-const StyledContainer = styled.div`
-  background-color: 'red';
-`; 
-*/
-
-const containerCSS = {
-  // backgroundColor: 'gray',
-  width: '700px',
-  margin: 'auto',
+const backgroundCSS = {
+  background: 'linear-gradient(-225deg, #2CD8D5 0%, #C5C1FF 56%, #FFBAC3 100%)',
+  height: '100vh',
 }
+
+const StyledHeader = styled.header`
+  height: 100px;
+  text-align: center;
+  font-size: 50px;
+  font-family: 'Cherry Swash', cursive;
+  padding-top: 30px;
+`;
+
+const StyledContainer = styled.div`
+  // background-color: white;
+  width: 700px;
+  margin: auto;
+  height: 70px;
+  text-align: center;
+  font-size: 20px;
+`; 
 
 function App() {
   return (
-    <div style={containerCSS}>
-      <SearchBox />
+    <div style={backgroundCSS}>
+      <StyledHeader>
+        Word Reputation Checker
+      </StyledHeader>
+      <StyledContainer>
+        <Typography variant="h6">
+          気になる単語を入力して、<br/>
+          単語に対する世間からのリアルタイムな評判を見てみよう！
+        </Typography>  
+        <SearchBox />
+      </StyledContainer>
     </div>
   );
 }
--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from "@mui/material/TextField";
+import { Box } from '@mui/material';
 
-const SearchBoxCSS = {
-    height: '40px',
-    width: '320px',
-    border: '10px',
-
-    margin: 'auto',
-    marginTop: '150px',
-
-    borderStyle: 'solid',
-    borderColor: 'black',
-    borderWidth: '4pt',
-}
+const StyledSearchBox = styled.div`
+  margin: 'auto';
+  margin-top: 50px;
+`;
 
 export const SearchBox = () => {
 
+  const [keyword, setKeyword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log(data.get('keyword'));
+  }
+
   return (
-    <div style={SearchBoxCSS}>
-        Box
-    </div>
+    <StyledSearchBox>
+      <Box component="form" onSubmit={handleSubmit}>
+        <TextField
+          id="keyword"
+          required
+          name="keyword"
+          onChange={(e) => {
+            setKeyword(e.target.value);
+          }}
+          variant="outlined"
+          placeholder="単語を入力"
+          size="small"
+          autoFocus
+        />
+        <IconButton type="submit">
+          <SearchIcon />
+        </IconButton>
+      </Box>
+    </StyledSearchBox>
   );
 };
