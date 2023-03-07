f0f2170d0bc16bb6872b3112a41996dd42c94bce
tkmtakada <tkmtakada@gmail.com>
2023-03-07 22:49:14 +0900

    3つのfetch_reputation系APIよういしました。

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,19 @@
 import os
+import re
 import json
-
+import emoji
+import base64
 import requests
+from io import BytesIO
 from dotenv import load_dotenv
-from fastapi import FastAPI, Response
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import FileResponse
-
-from starlette.middleware.cors import CORSMiddleware # 追加
-
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from fastapi import FastAPI, Response
+from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse
+from fastapi.encoders import jsonable_encoder
+from starlette.middleware.cors import CORSMiddleware # 追加
 # from janome.tokenizer import Tokenizer
-from io import BytesIO
-import base64
 # from pdb import set_trace as db
 
 
@@ -33,21 +32,124 @@ headers = {
 # 訓練済みBertをロードする
 if os.environ.get("USER") == 'takumi':
     # from mbti_classifier.mbti_classifier import *
+    print("!!! NOW DEGUB MODE. MAKE SURE TO LOAD MBTI BERT MODEL FOR PROD MODE. !!!")
     model = lambda x : "INFP"  # MBTIClassifier()
 else:
     model = lambda x : "INFP"  # 常にINFPを返すダミーモデル
 
 app = FastAPI()
 
-# CORSを回避するために追加（今回の肝）
+# CORSを回避するために追加
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,   # 追記により追加
-    allow_methods=["*"],      # 追記により追加
-    allow_headers=["*"]       # 追記により追加
+    allow_credentials=True, 
+    allow_methods=["*"],    
+    allow_headers=["*"]     
 )
 
+# ===========================
+#    DEFINE UTILITY FUNCTION
+# ===========================
+# ツイートを検索
+def get_tweet(q):
+    # endpointに付けるパラメータ
+    params = {"q": q}
+
+    # リクエストを送信し、応答を取得
+    response = requests.request("GET", url=search_endpoint, params=params, headers=headers)
+    if response.status_code != 200:
+        raise Exception("Request returned an error: {} {}".format(response.status_code, response.text))
+    return response.text
+
+
+
+def get_tweets_about_trend():
+    params = {"id": 23424856}
+
+    response = requests.request("GET", url=trend_endpoint, params=params, headers=headers)
+    if response.status_code != 200:
+        raise Exception("Request returned an error: {} {}".format(response.status_code, response.text))
+    res = json.loads(response.text)
+    print("trend res: ", res)
+    
+    
+    # トレンド単語を10個抽出
+    related_tweets = []
+    counter = 0
+    trend_word_list = []
+    for r in res[0]["trends"]:
+        word = r["name"]
+        # トレンド単語に関するツイートを検索
+        print("trend word: ", word)
+        related_tweets.append(get_tweet(word))
+        trend_word_list.append(word)
+        counter += 1
+        if counter >= 10: break
+
+    print("related_tweets", json.loads(related_tweets[0]).keys())
+    json_tweet = json.loads(related_tweets[0])
+    print("related_tweets", json_tweet["statuses"][0]["text"])
+
+    # db()
+
+    tweets_list = []
+    for str_tweet_list_per_keyword in related_tweets:
+        tweet_list_per_keyword = json.loads(str_tweet_list_per_keyword)["statuses"]
+        for tweet_info in tweet_list_per_keyword:
+            tweet = tweet_info["text"]
+            # --- 文章中のURL を除去 ---
+            tweet = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)", "" ,tweet)
+            # --- remove emoji ---
+            tweet = emoji.replace_emoji(tweet)
+            # メンション除去 
+            tweet = re.sub(r"@(\w+) ", "", tweet)
+            #リツイートを消す
+            tweet = re.sub(r"(^RT.*)", "", tweet, flags=re.MULTILINE | re.DOTALL)
+            # 不要記号削除
+            pattern = '[!"#$%&\'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”◇ᴗ●↓→♪★⊂⊃※△□◎〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]' 
+            tweet = re.sub(pattern, ' ', tweet)
+            
+            tweets_list.append(tweet)
+            # print(t["statuses"]["text"])
+
+    return tweets_list, trend_word_list
+
+
+
+# sentence --> byte data of PNG word cloud image
+def generate_wordcloud(sentence_list):
+    if os.environ.get("USER") == 'riki':        
+        fpath='/Users/riki/Downloads/ipag00303/ipag.ttf'#日本語設定＃
+    elif os.environ.get("USER") == 'takumi':
+        fpath='/home/takumi/Downloads/ipag00303/ipag.ttf'#日本語設定＃
+    text = "".join(sentence_list)
+    wordcloud=WordCloud(
+    height=600,
+    width=900,
+    background_color="white",
+    max_words=6,
+    min_font_size=40,
+    max_font_size=200,
+    collocations=False,
+    font_path=fpath,
+    ).generate(text)
+    plt.figure(figsize=(15,12))
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    figfile = BytesIO()
+    plt.savefig(figfile)
+    byteData = figfile.getvalue()
+    base64data = base64.b64encode(byteData).decode()
+    print("base64 string; ", base64data)
+    return base64data
+
+    # return JSONResponse({"image":base64.b64encode(bdata).decode()})
+
+
+# ===========================
+#    DEFINE CONTROLLOERS
+# ===========================
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
@@ -102,17 +204,6 @@ async def predict_mbti(text = "Hope!"):
     print("PREDICTED MBTI: ", output)
     return JSONResponse({"mbti": output})
 
-# ツイートを検索
-def get_tweet(q):
-    # endpointに付けるパラメータ
-    params = {"q": q}
-
-    # リクエストを送信し、応答を取得
-    response = requests.request("GET", url=search_endpoint, params=params, headers=headers)
-    if response.status_code != 200:
-        raise Exception("Request returned an error: {} {}".format(response.status_code, response.text))
-    return response.text
-
 
 @app.get("/get_trend")
 def get_trend():
@@ -147,6 +238,10 @@ def get_trend():
         tweet_list_per_keyword = json.loads(str_tweet_list_per_keyword)["statuses"]
         for tweet_info in tweet_list_per_keyword:
             tweet = tweet_info["text"]
+            # --- 文章中のURL を除去 ---
+            tweet = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)", "" ,tweet)
+            # --- remove emoji ---
+            tweet = emoji.replace_emoji(string)
             tweets_list.append(tweet)
             # print(t["statuses"]["text"])
 
@@ -158,6 +253,34 @@ def fetch_reputation_data_by_keywords(keywords : str):
     1 関連するツイートを取得
     2 ワードクラウドをつくる＆画像をかえす
     3 MBTIをかえす
+    """
+    tweets_info_list = json.loads(get_tweet(keywords))
+    tweets_list = []
+    for tweet_info in tweets_info_list["statuses"]:
+        # print(len(tweets_info_list["statuses"]))
+        # print(tweets_info_list["statuses"][0]["text"])
+        tweet = tweet_info["text"]
+        # --- 文章中のURL を除去 ---
+        tweet = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)", "" ,tweet)
+        # --- remove emoji ---
+        tweet = emoji.replace_emoji(tweet)
+        # メンション除去 
+        tweet = re.sub(r"@(\w+) ", "", tweet)
+        #リツイートを消す
+        tweet = re.sub(r"(^RT.*)", "", tweet, flags=re.MULTILINE | re.DOTALL)
+        # 不要記号削除
+        pattern = '[!"#$%&\'\\\\()*+,-./:;<=>?@[\\]^_`{|}~「」〔〕“”◇ᴗ●↓→♪★⊂⊃※△□◎〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％]' 
+        tweet = re.sub(pattern, ' ', tweet)
+        tweets_list.append(tweet)
+    
+    wordcloud_image = generate_wordcloud(tweets_list)
+    mbti_list = [model(tweet) for tweet in tweets_list]
+    return JSONResponse({"image": wordcloud_image,
+                         "tweets_list": tweets_list,
+                         "mbti_list": mbti_list})
+    
+    
+    # #######################
     """
     print(keywords)
     res = get_wordcloud("a b c d ef f")
@@ -191,3 +314,24 @@ def fetch_reputation_data_by_keywords(keywords : str):
 
     return JSONResponse({"mbti": "INFP",
                         "image": b64str })
+    """
+
+
+@app.get("/fetch_reputation_data_by_sentence")
+def fetch_reputation_data_by_sentence(sentence: str):
+    # word_cloud, mbti
+    wordcloud_image = generate_wordcloud([sentence])
+    mbti = model(sentence)
+    return JSONResponse({"image": wordcloud_image,
+                         "mbti": mbti})
+
+@app.get("/fetch_reputation_data_by_trend")
+def fetch_reputation_data_by_trend():
+    tweets_list, trend_word_list = get_tweets_about_trend()
+    wordcloud_image = generate_wordcloud(tweets_list)
+    mbti_list = [model(tweet) for tweet in tweets_list]
+    mbti_all = model("".join(tweets_list))
+    return JSONResponse({"image": wordcloud_image,
+                         "tweet":tweets_list,
+                         "mbti":mbti_list,
+                         "mbti_all": mbti_all})
