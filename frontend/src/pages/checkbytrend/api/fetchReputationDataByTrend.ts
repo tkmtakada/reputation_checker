0caf5877a60f63b4f94176cb174e9f176153e7fb
Koichi Takato <koichi.taka.0818@gmail.com>
2023-03-18 13:27:58 +0900

    change; urlの定義方法変更

--- frontend/src/pages/checkbytrend/api/fetchReputationDataByTrend.ts
+++ frontend/src/pages/checkbytrend/api/fetchReputationDataByTrend.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { responseByTrend } from 'types/response';
 
-const baseURL = process.env.REACT_APP_BACKEND_API_URL;
+// const baseURL = process.env.REACT_APP_BACKEND_API_URL;
+const baseURL = "http://127.0.0.1:8000";
 
 export const fetchReputationDataByTrend = (setPost: (post: responseByTrend) => void) => {
   axios
